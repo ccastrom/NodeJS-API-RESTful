1b8517d9b748bde49fb3f7cfce7c818e641726a6
BRAVOSIX <claudio_jr@live.cl>
2021-08-17 21:54:00 -0400

    Se comenta los pasos para mayor comprensión

--- 05-Express01/app.js
+++ 05-Express01/app.js
@@ -2,22 +2,34 @@ const express=require('express');
 const app=express();
 const Joi=require('joi');
 app.use(express.json());
+/**
+ * Se define un arreglo con usuarios genericos
+ */
 const usuarios= [{id:1,nombre:"Claudio"},
                  {id:2,nombre:"Pepe"},
                  {id:3,nombre:"Jose"}];
 
+/**
+ * Primera petición para obtener información!
+ */
 app.get('/',(req,res)=>{
 res.send("Hola mundo desde Express.");
 });
-
+/**Petición get para obtener como respuesta una lista de usuarios definidas */
 app.get('/api/usuarios',(req,res)=>{
     res.send(['grover','luis','anna','frank']);
 })
+/**
+ * Petición get para obtener un usuario de la lista definida según un parametro
+ */
 app.get('/api/usuarios/:id',(req,res)=>{
   let usuario=usuarios.find(u=>u.id== parseInt(req.params.id));
   if(!usuario) res.status(404).send('El usuario no fue encontrado');
   res.send(usuario);
 })
+/**
+ * Petición para añadir un usuario nueva a la lista, se valida los valores utilizando Joi
+ */
 app.post('/api/usuarios',(req,res)=>{
     const schema = Joi.object({
         nombre: Joi.string().min(3).required()
@@ -28,25 +40,13 @@ app.post('/api/usuarios',(req,res)=>{
             id:usuarios.length+1,
             nombre:value
         };
-        usuarios.push(usuario);
+        usuarios.push(usuario);//Añadir usuario a la lisa
         res.send(usuario);
         
     }else{
         const message=error.details[0].message;
         res.status(400).send(message);
     }
-
-           
-          
-           
-           
-
-    // if(!req.body.nombre ||req.body.nombre.length<=2){
-    //     //400 Bad Request
-    //     res.status(400).send('Debe ingresar un nombre, que tenga minimo 3 letras');
-    //     return;
-    // }
-   
 })
 const port =process.env.PORT || 3000
 app.listen(port,()=>{
